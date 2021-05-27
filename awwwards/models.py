c8a1dad2b335c8fc4abe1d837ab582f18b47a6f4
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-27 08:04:14 +0300

    Created Profile Model

--- awwwards/models.py
+++ awwwards/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_picture = CloudinaryField('image',null=True, default='default.png')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    contact = models.EmailField(max_length=100, blank=True)
