d7b0f3beac8d9d00c986685ab44b15451c4630b3
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-27 10:28:26 +0300

    Implemented a form and modal to enable a user to submit

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import ProjectsForm
+from .models import Projects,Profile
 
 # Create your views here.
 @login_required (login_url='/accounts/login/')
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        form = ProjectsForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = request.user
+            project.save()
+    else:
+        form = ProjectsForm()
+
+    return render(request, 'index.html', {'form': form})
