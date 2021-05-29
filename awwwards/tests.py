afc5e2a7cd8a5f5b8a9df24de4412c8351476244
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-29 15:42:01 +0300

    Updated tests

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -17,10 +17,10 @@ class TestProfile(TestCase):
     def test_delete_user(self):
         self.user.delete()
 
-class PostTest(TestCase):
+class ProjectsTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(id=1, username='derrick')
-        self.project = Projects.objects.create(id=1, title='test post', photo='https://images.pexels.com/photos/1974385/pexels-photo-1974385.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', description='test description',
+        self.project = Projects.objects.create(id=1, title='test project', photo='https://images.pexels.com/photos/1974385/pexels-photo-1974385.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', description='test description',
                                         user=self.user, url='https://www.pexels.com/photo/grayscale-photo-of-a-woman-in-a-garden-1974385/')
 
     def test_instance(self):
@@ -45,3 +45,24 @@ class PostTest(TestCase):
         self.project.delete_project()
         project = Projects.search_project('test')
         self.assertTrue(len(project) < 1)
+
+
+class RatingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='derrick')
+        self.project = Projects.objects.create(id=1, title='test project', photo='https://images.pexels.com/photos/1974385/pexels-photo-1974385.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', description='test description',
+                                        user=self.user, url='https://www.pexels.com/photo/grayscale-photo-of-a-woman-in-a-garden-1974385/')
+        self.rating = Rating.objects.create(id=1, design=6, usability=7, content=9, user=self.user, projects=self.projects)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
+
+    def test_save_rating(self):
+        self.rating.save_rating()
+        rating = Rating.objects.all()
+        self.assertTrue(len(rating) > 0)
+
+    def test_get_project_rating(self, id):
+        self.rating.save()
+        rating = Rating.get_ratings(project_id=id)
+        self.assertTrue(len(rating) == 1)
