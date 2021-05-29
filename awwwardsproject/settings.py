e77ad955a27735c73a412127ddcf96d82a9ee79f
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-29 16:49:17 +0300

    Created env file

--- awwwardsproject/settings.py
+++ awwwardsproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ct36dm%8ydtn_@w1^ug=l+@3$ow!357nd_1)=#adkgs=tg@$%j'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
